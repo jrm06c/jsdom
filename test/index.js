f858c3af41423a3b6db0cef3cc5203cdef68e10c
Sebastian Mayr <git@smayr.name>
2017-09-10 18:42:14 +0200

    Migrate level2/html to mocha

--- test/index.js
+++ test/index.js
@@ -39,6 +39,7 @@ require("./to-port-to-wpts/level1/html.js");
 
 require("./to-port-to-wpts/level2/core.js");
 require("./to-port-to-wpts/level2/events.js");
+require("./to-port-to-wpts/level2/html.js");
 
 require("./web-platform-tests/to-upstream.js");
 // keep this one last so that you can cancel the test run early
