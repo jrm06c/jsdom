0cf3f15c91c41372b7e83a5a58357dc847f28359
Sebastian Mayr <git@smayr.name>
2017-09-11 01:02:18 +0200

    Migrate htmlelement to mocha

--- test/index.js
+++ test/index.js
@@ -33,6 +33,7 @@ require("./to-port-to-wpts/frame.js");
 require("./to-port-to-wpts/history.js");
 require("./to-port-to-wpts/htmlanchorelement.js");
 require("./to-port-to-wpts/htmlcanvaselement.js");
+require("./to-port-to-wpts/htmlelement.js");
 require("./to-port-to-wpts/htmlimageelement.js");
 require("./to-port-to-wpts/jsonp.js");
 require("./to-port-to-wpts/misc.js");
