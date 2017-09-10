c3bd3dd5d2c5631cbe9b10612e4aade17ea3e43a
Sebastian Mayr <git@smayr.name>
2017-09-11 00:18:41 +0200

    Migrate htmlanchorelement to mocha

--- test/index.js
+++ test/index.js
@@ -31,6 +31,7 @@ require("./to-port-to-wpts/current-script.js");
 require("./to-port-to-wpts/dom-implementation.js");
 require("./to-port-to-wpts/frame.js");
 require("./to-port-to-wpts/history.js");
+require("./to-port-to-wpts/htmlanchorelement.js");
 require("./to-port-to-wpts/jsonp.js");
 require("./to-port-to-wpts/misc.js");
 require("./to-port-to-wpts/misc2.js");
