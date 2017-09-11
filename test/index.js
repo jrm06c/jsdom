e51b8440a7633c11eae384bbb4d5dda966975eca
Sebastian Mayr <git@smayr.name>
2017-09-12 01:29:26 +0200

    Migrate xpath to mocha

--- test/index.js
+++ test/index.js
@@ -73,6 +73,7 @@ require("./to-port-to-wpts/level2/html.js");
 require("./to-port-to-wpts/level2/style.js");
 
 require("./to-port-to-wpts/level3/textContent.js");
+require("./to-port-to-wpts/level3/xpath.js");
 
 require("./web-platform-tests/to-upstream.js");
 // keep this one last so that you can cancel the test run early
